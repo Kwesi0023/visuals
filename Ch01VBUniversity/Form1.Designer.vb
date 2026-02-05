fb5dc48e78b182cbf30288b3bb7542b7da03ba27
Kwesi0023 <humphreykusi@gmail.com>
2026-02-05 22:23:12 +0000

    so far so okayy

--- Ch01VBUniversity/Form1.Designer.vb
+++ Ch01VBUniversity/Form1.Designer.vb
@@ -175,6 +175,13 @@ Partial Class Form1
         NameTextBox.Clear()
         MajorTextBox.Clear()
     End Sub
+    Private Sub PrintButton_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
 
 End Class
