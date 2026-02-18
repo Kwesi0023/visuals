ea78759cfff956385d6d6701fb4dde8819d68d26
Kwesi0023 <humphreykusi@gmail.com>
2026-02-18 21:17:15 +0000

    the classwork

---
+++ CLASSPROJECTS/CLASSPROJECTS/Form1.Designer.vb
@@ -0,0 +1,492 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        CheckBox1 = New CheckBox()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox2 = New GroupBox()
+        TextBox10 = New TextBox()
+        Me.txtTradeIn = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox5 = New TextBox()
+        txtPrice = New TextBox()
+        Label10 = New Label()
+        Label9 = New Label()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label5 = New Label()
+        Label4 = New Label()
+        GroupBox3 = New GroupBox()
+        RadioButton4 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        GroupBox4 = New GroupBox()
+        CheckBox5 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        BtnCompute = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        BtnExit = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(CheckBox1)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(24, 25)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(520, 130)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Used Automative Information"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(130, 63)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(370, 23)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(384, 20)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(116, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(89, 20)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(203, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(190, 92)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(166, 19)
+        CheckBox1.TabIndex = 3
+        CheckBox1.Text = "Wholesale Dealer Discount"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(346, 23)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(32, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Year:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(6, 71)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(118, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "&Vehicle Make/Model:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(74, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "&Lot Number:"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(TextBox10)
+        GroupBox2.Controls.Add(Me.txtTradeIn)
+        GroupBox2.Controls.Add(TextBox8)
+        GroupBox2.Controls.Add(TextBox7)
+        GroupBox2.Controls.Add(TextBox6)
+        GroupBox2.Controls.Add(TextBox5)
+        GroupBox2.Controls.Add(txtPrice)
+        GroupBox2.Controls.Add(Label10)
+        GroupBox2.Controls.Add(Label9)
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(Label4)
+        GroupBox2.Location = New Point(24, 170)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(292, 260)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Sales Information"
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(70, 220)
+        TextBox10.Name = "TextBox10"
+        TextBox10.ReadOnly = True
+        TextBox10.Size = New Size(204, 23)
+        TextBox10.TabIndex = 13
+        ' 
+        ' txtTradeIn
+        ' 
+        Me.txtTradeIn.Location = New Point(70, 190)
+        Me.txtTradeIn.Name = "txtTradeIn"
+        Me.txtTradeIn.Size = New Size(204, 23)
+        Me.txtTradeIn.TabIndex = 12
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(70, 161)
+        TextBox8.Name = "TextBox8"
+        TextBox8.ReadOnly = True
+        TextBox8.Size = New Size(204, 23)
+        TextBox8.TabIndex = 11
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(70, 126)
+        TextBox7.Name = "TextBox7"
+        TextBox7.ReadOnly = True
+        TextBox7.Size = New Size(204, 23)
+        TextBox7.TabIndex = 10
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(70, 96)
+        TextBox6.Name = "TextBox6"
+        TextBox6.ReadOnly = True
+        TextBox6.Size = New Size(204, 23)
+        TextBox6.TabIndex = 9
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(70, 62)
+        TextBox5.Name = "TextBox5"
+        TextBox5.ReadOnly = True
+        TextBox5.Size = New Size(204, 23)
+        TextBox5.TabIndex = 8
+        ' 
+        ' txtPrice
+        ' 
+        txtPrice.Location = New Point(70, 31)
+        txtPrice.Name = "txtPrice"
+        txtPrice.Size = New Size(204, 23)
+        txtPrice.TabIndex = 7
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(6, 228)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(60, 15)
+        Label10.TabIndex = 6
+        Label10.Text = "Total Due:"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(16, 194)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(54, 15)
+        Label9.TabIndex = 5
+        Label9.Text = "Trade-In:"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(16, 164)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(56, 15)
+        Label8.TabIndex = 4
+        Label8.Text = "Sales Tax:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(16, 132)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(54, 15)
+        Label7.TabIndex = 3
+        Label7.Text = "Subtotal:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(16, 99)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(40, 15)
+        Label6.TabIndex = 2
+        Label6.Text = "Extras:"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(16, 65)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(57, 15)
+        Label5.TabIndex = 1
+        Label5.Text = "Discount:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(16, 34)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(36, 15)
+        Label4.TabIndex = 0
+        Label4.Text = "&Price:"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Controls.Add(RadioButton4)
+        GroupBox3.Controls.Add(RadioButton3)
+        GroupBox3.Controls.Add(RadioButton2)
+        GroupBox3.Controls.Add(RadioButton1)
+        GroupBox3.Location = New Point(345, 170)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(200, 127)
+        GroupBox3.TabIndex = 2
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Exterior Extas"
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(6, 97)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(50, 19)
+        RadioButton4.TabIndex = 3
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Both"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(6, 72)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(80, 19)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Undercoat"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(6, 47)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(109, 19)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Point Touch-Up"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(6, 22)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(54, 19)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "None"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.Controls.Add(CheckBox5)
+        GroupBox4.Controls.Add(CheckBox4)
+        GroupBox4.Controls.Add(CheckBox3)
+        GroupBox4.Controls.Add(CheckBox2)
+        GroupBox4.Location = New Point(346, 303)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(199, 127)
+        GroupBox4.TabIndex = 3
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "Accessory E&xtras"
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(5, 95)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(109, 19)
+        CheckBox5.TabIndex = 3
+        CheckBox5.Text = "New Floor Mats"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(5, 72)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(89, 19)
+        CheckBox4.TabIndex = 2
+        CheckBox4.Text = "Built-in &GPS"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(5, 47)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(103, 19)
+        CheckBox3.TabIndex = 1
+        CheckBox3.Text = "New H&D Radio"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(6, 22)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(78, 19)
+        CheckBox2.TabIndex = 0
+        CheckBox2.Text = "New Ti&res"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' BtnCompute
+        ' 
+        BtnCompute.Location = New Point(66, 470)
+        BtnCompute.Name = "BtnCompute"
+        BtnCompute.Size = New Size(75, 23)
+        BtnCompute.TabIndex = 4
+        BtnCompute.Text = "&Compute"
+        BtnCompute.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(165, 470)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "&Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(260, 470)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 6
+        Button3.Text = "&Totals"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' BtnExit
+        ' 
+        BtnExit.Location = New Point(355, 470)
+        BtnExit.Name = "BtnExit"
+        BtnExit.Size = New Size(75, 23)
+        BtnExit.TabIndex = 7
+        BtnExit.Text = "E&xit"
+        BtnExit.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(587, 522)
+        Controls.Add(BtnExit)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(BtnCompute)
+        Controls.Add(GroupBox4)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Rock Repair Parts and Automative Sales"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents BtnCompute As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents txtPrice As TextBox
+
+End Class
---
+++ CLASSPROJECTS/CLASSPROJECTS/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub BtnCompute_Click(sender As Object, e As EventArgs) Handles BtnCompute.Click
+        Dim price As Decimal = 0
+        Dim tradeIn As Decimal = 0
+        Dim extras As Decimal = 0
+        Dim subtotal As Decimal = 0
+        Dim tax As Decimal = 0
+        Dim totalDue As Decimal = 0
+        Dim DiscountAmount As Decimal = 0
+        Const DISCOUNT_RATE As Decimal = 0.01D
+        Const TAX_RATE As Decimal = 0.08D
+
+        Decimal.TryParse(txtPrice.Text, price)
+        Decimal.TryParse(txtTradeIn.Text, tradeIn)
+
+        If RadTouchUp.Cheked Then
+            extras += 50D
+        ElseIf radUndercoat.checked Then
+            extras += 100D
+        ElseIf radboth.checked Then
+            extras += 150D
+        End If
+
+        If chkWholesale.Checked Then
+            DiscountAmount = price * DISCOUNT_RATE
+        Else
+            DiscountAmount = 0
+        End If
+
+        If Accessory1.Checked Then
+            extras += 25D
+        End If
+
+        subtotal = price - tradeIn - DiscountAmount + extras
+        tax = subtotal * TAX_RATE
+        totalDue = subtotal + tax
+
+        lblSubtotal.Text = subtotal.ToString("C")
+        lbltax.Text = tax.ToString("C")
+        lblDiscount.Text = DiscountAmount.ToString("C")
+        lblExtras.Text = extras.ToString("C")
+        lblTotalDue.Text = totalDue.ToString("C")
+    End Sub
+
+End Class
