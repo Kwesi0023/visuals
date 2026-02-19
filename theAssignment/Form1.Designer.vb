9b45f0e8c549ccd9a6e715cf4b7bb95efc11f5ed
Kwesi0023 <humphreykusi@gmail.com>
2026-02-19 02:30:14 +0000

    small scale payment system

---
+++ theAssignment/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtPieces = New System.Windows.Forms.TextBox()
+        Me.BtnCalculate = New System.Windows.Forms.Button()
+        Me.BtnSummary = New System.Windows.Forms.Button()
+        Me.BtnClear = New System.Windows.Forms.Button()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.label0 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(137, 41)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(153, 20)
+        Me.txtName.TabIndex = 0
+        '
+        'txtPieces
+        '
+        Me.txtPieces.Location = New System.Drawing.Point(144, 84)
+        Me.txtPieces.Name = "txtPieces"
+        Me.txtPieces.Size = New System.Drawing.Size(146, 20)
+        Me.txtPieces.TabIndex = 1
+        '
+        'BtnCalculate
+        '
+        Me.BtnCalculate.Location = New System.Drawing.Point(47, 367)
+        Me.BtnCalculate.Name = "BtnCalculate"
+        Me.BtnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCalculate.TabIndex = 2
+        Me.BtnCalculate.Text = "&Calculate"
+        Me.BtnCalculate.UseVisualStyleBackColor = True
+        '
+        'BtnSummary
+        '
+        Me.BtnSummary.Location = New System.Drawing.Point(166, 367)
+        Me.BtnSummary.Name = "BtnSummary"
+        Me.BtnSummary.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSummary.TabIndex = 3
+        Me.BtnSummary.Text = "Sum&mary"
+        Me.BtnSummary.UseVisualStyleBackColor = True
+        '
+        'BtnClear
+        '
+        Me.BtnClear.Location = New System.Drawing.Point(287, 367)
+        Me.BtnClear.Name = "BtnClear"
+        Me.BtnClear.Size = New System.Drawing.Size(75, 23)
+        Me.BtnClear.TabIndex = 4
+        Me.BtnClear.Text = "Cl&ear"
+        Me.BtnClear.UseVisualStyleBackColor = True
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Location = New System.Drawing.Point(418, 367)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(75, 23)
+        Me.BtnClose.TabIndex = 5
+        Me.BtnClose.Tag = ""
+        Me.BtnClose.Text = "&Close"
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'label0
+        '
+        Me.label0.AutoSize = True
+        Me.label0.Location = New System.Drawing.Point(44, 164)
+        Me.label0.Name = "label0"
+        Me.label0.Size = New System.Drawing.Size(40, 13)
+        Me.label0.TabIndex = 6
+        Me.label0.Text = "Result:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "&Employee Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Number of &Pieces:"
+        '
+        'lblResult
+        '
+        Me.lblResult.Location = New System.Drawing.Point(137, 161)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.ReadOnly = True
+        Me.lblResult.Size = New System.Drawing.Size(153, 20)
+        Me.lblResult.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(577, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.label0)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.BtnClear)
+        Me.Controls.Add(Me.BtnSummary)
+        Me.Controls.Add(Me.BtnCalculate)
+        Me.Controls.Add(Me.txtPieces)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "Form1"
+        Me.Text = "Pieces Work Payment System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtPieces As TextBox
+    Friend WithEvents BtnCalculate As Button
+    Friend WithEvents BtnSummary As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnClose As Button
+    Friend WithEvents label0 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblResult As TextBox
+End Class
---
+++ theAssignment/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Dim response As DialogResult
+        response = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If response = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private totalPiecesAll As Integer = 0
+    Private totalPayAll As Decimal = 0
+    Private personCount As Integer = 0
+
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles BtnCalculate.Click
+        If txtName.Text = "" Or Not IsNumeric(txtPieces.Text) Then
+            MessageBox.Show("Please enter a valid name and number of pieces.")
+            Return
+        End If
+
+        Dim pieces As Integer = CInt(txtPieces.Text)
+        Dim rate As Decimal
+
+        If pieces <= 200 Then
+            rate = 0.6D
+        ElseIf pieces <= 400 Then
+            rate = 0.65D
+        ElseIf pieces <= 600 Then
+            rate = 0.7D
+        Else
+            rate = 0.75D
+        End If
+
+        Dim amountEarned As Decimal = pieces * rate
+        lblResult.Text = amountEarned.ToString("C")
+
+        totalPiecesAll += pieces
+        totalPayAll += amountEarned
+        personCount += 1
+
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        txtName.Clear()
+        txtPieces.Clear()
+        lblResult.Text = ""
+
+        txtName.Focus()
+    End Sub
+
+    Private Sub BtnSummary_Click(sender As Object, e As EventArgs) Handles BtnSummary.Click
+        If personCount > 0 Then
+            Dim averagePay As Decimal = totalPayAll / personCount
+            Dim message As String = "Total Pieces: " & totalPiecesAll.ToString() & vbCrLf &
+                                    "Total Pay: " & totalPayAll.ToString("C") & vbCrLf &
+                                    "Average Pay per Person: " & averagePay.ToString("C")
+
+
+            MessageBox.Show(message, "Summary Totals", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("No data has been calculated yet.", "Summary Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+
+    End Sub
+
+    ' vbCrLf-- moves to a newline like "std::cin>>"'
+End Class
